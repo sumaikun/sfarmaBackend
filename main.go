77fae9bce7d126d5f3d6cf8fc1d25136db572766
sumaikun <ventas.javc@gmail.com>
2020-04-15 15:12:30 -0500

    cors working properly

--- main.go
+++ main.go
@@ -42,13 +42,19 @@ type CORSRouterDecorator struct {
 // ServeHTTP wraps the HTTP server enabling CORS headers.
 // For more info about CORS, visit https://www.w3.org/TR/cors/
 func (c *CORSRouterDecorator) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	if origin := req.Header.Get("Origin"); origin != "" {
-		rw.Header().Set("Access-Control-Allow-Origin", origin)
-		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		rw.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, YourOwnHeader")
-	}
+
+	//fmt.Println("I am on serve HTTP")
+
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+
+	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+
 	// Stop here if its Preflighted OPTIONS request
 	if req.Method == "OPTIONS" {
+		//fmt.Println("I am in options")
+		rw.WriteHeader(http.StatusOK)
 		return
 	}
 
